447a51a5abb7503e8192c55e5e1acb44d49f690c
dishantrahangdale <dishant.rahangdale@gmail.com>
2022-01-26 21:58:37 +0530

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 // import logo from './logo.svg';
 import './App.css';
+import './Footer.css'
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import Footer from './components/Footer';
 import react, {useState} from 'react';
 function App() {
   return (
     // <Navbar/>
     <>
-    <Navbar title="TextUtils" aboutText = "About"    />    
+    <Navbar title="Textutils" aboutText = "About"    />    
     <div className="container">
     <TextForm heading = "Enter the text to analyze below"  />
     </div>
-    
+    <Footer/>
     </>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+// import './'
+
+export default function footer() {
+  return <div>
+      <footer className='footer'>Made By Dishant Rahangdale</footer>
+  </div>;
+}
